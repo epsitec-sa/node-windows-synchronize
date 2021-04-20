6b117c4d133bb93601ac113bbdcfd98357ba4e72
Jonny Quarta <quarta@epsitec.ch>
2021-04-20 13:23:35 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,66 @@
 /*global describe,it*/
 
 var assert = require("assert");
+const { mutexAccess } = require("../");
 var lib = require("../");
 
-describe("ExecuteCommand", function () {
-  it("should receive command", function () {
-    // TODO: start udater service
-    lib.executeCommand("CresusUpdaterService", 200); // CheckAvailable command
-  });
+describe("CreateMutex", function () {
+  it("should create mutex", function () {
+    const handle = lib.createMutex("Local\\TestMutex");
 
-  it("should not connect to service", function () {
-    assert.throws(() => lib.executeCommand("DummyService", 200));
+    assert.ok(handle);
+
+    lib.closeMutex(handle);
+  });
+});
+
+describe("OpenMutex", function () {
+  it("should create and open mutex", function () {
+    const cHandle = lib.createMutex("Local\\TestMutex");
+    const oHandle = lib.openMutex(
+      "Local\\TestMutex",
+      lib.mutexAccess.Synchronize
+    );
+
+    assert.ok(cHandle);
+    assert.ok(oHandle);
+
+    lib.closeMutex(oHandle);
+    lib.closeMutex(cHandle);
+  });
+});
+
+describe("WaitReleaseMutex", function () {
+  it("should create and wait/release mutex", function () {
+    const handle = lib.createMutex("Local\\TestMutex");
+
+    assert.ok(handle);
+
+    lib.waitMutex(handle, lib.waitTime.Infinite);
+    lib.releaseMutex(handle);
+
+    lib.closeMutex(handle);
+  });
+});
+
+describe("MultipleWaitReleaseMutex", function () {
+  it("should create, open and wait/release mutex by multiple instances", function () {
+    const cHandle = lib.createMutex("Local\\TestMutex");
+    const oHandle = lib.openMutex(
+      "Local\\TestMutex",
+      lib.mutexAccess.Synchronize
+    );
+
+    assert.ok(cHandle);
+    assert.ok(oHandle);
+
+    lib.waitMutex(oHandle, lib.waitTime.Infinite);
+    lib.releaseMutex(oHandle);
+
+    lib.waitMutex(cHandle, lib.waitTime.Infinite);
+    lib.releaseMutex(cHandle);
+
+    lib.closeMutex(oHandle);
+    lib.closeMutex(cHandle);
   });
 });
